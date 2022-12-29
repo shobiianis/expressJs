e768a6a6ad5c8d9cae3a52c965e14bc108aa4672
shoaib anis <shoaibwork2000@gmail.com>
2022-12-30 00:56:11 +0500

    third tasks

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const path=require("path");
 const app=express();
 const pathForIndexFile=path.join(__dirname,"public");
 
-app.use(express.static(pathForIndexFile));
+//app.use(express.static(pathForIndexFile)); if we want to remove extension so this command is not used
+app.get("/about",(req,res)=>{
+res.sendFile(`${pathForIndexFile}/about.html`)
+
+})
 app.listen(8001);
